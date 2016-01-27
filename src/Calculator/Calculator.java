bd4251f778a83451d7602f9ea6747f16551690bf
namlai000 <theguncrusher@gmail.com>
2016-01-27 20:28:23 +0700

    Fix Header

--- src/Calculator/Calculator.java
+++ src/Calculator/Calculator.java
@@ -1,9 +1,5 @@
-﻿/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Calculator;
+
 import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
